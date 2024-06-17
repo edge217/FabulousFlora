26c2b71414410396c931b9f29b20976e488dd251
Edgeburn <edgeburn02@mailredirect.edgeburnmedia.com>
2024-06-17 18:10:51 -0400

    add blocks

--- common/src/main/java/net/watsonrolfes/fabulousflora/FabulousFlora.java
+++ common/src/main/java/net/watsonrolfes/fabulousflora/FabulousFlora.java
@@ -4,10 +4,53 @@
 
 package net.watsonrolfes.fabulousflora;
 
+import dev.architectury.event.events.client.ClientLifecycleEvent;
+import dev.architectury.registry.CreativeTabRegistry;
+import dev.architectury.registry.client.rendering.RenderTypeRegistry;
+import dev.architectury.registry.registries.DeferredRegister;
+import dev.architectury.registry.registries.RegistrySupplier;
+import dev.architectury.utils.Env;
+import dev.architectury.utils.EnvExecutor;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.text.Text;
+import net.watsonrolfes.fabulousflora.registry.FFBlocks;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public final class FabulousFlora {
     public static final String MOD_ID = "fabulous_flora";
+    private static final Logger log = LoggerFactory.getLogger(FabulousFlora.class);
+    public static final DeferredRegister<ItemGroup> TABS = DeferredRegister.create(MOD_ID, RegistryKeys.ITEM_GROUP);
+    public static final RegistrySupplier<ItemGroup> FF_CREATIVE_TAB = TABS.register("creative_tab",
+            () -> CreativeTabRegistry.create(Text.translatable("itemGroup.fabulousFlora"),
+                    () -> new ItemStack(FFBlocks.PURPLE_IRIS.get())));
 
     public static void init() {
-        // Write common init code here.
+
+        FFBlocks.register();
+        TABS.register();
+
+        EnvExecutor.runInEnv(Env.CLIENT, () -> FFClient::initClient);
+    }
+
+    @Environment(EnvType.CLIENT)
+    public static class FFClient {
+        @Environment(EnvType.CLIENT)
+        public static void initClient() {
+            ClientLifecycleEvent.CLIENT_STARTED.register(instance -> {
+                FFBlocks.BLOCKS.forEach(blockRegistrySupplier -> {
+                    Block block = blockRegistrySupplier.get();
+                    RenderTypeRegistry.register(RenderLayer.getTranslucent(), block);
+                });
+            });
+
+        }
     }
 }
---
+++ common/src/main/java/net/watsonrolfes/fabulousflora/registry/FFBlocks.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2024 Edgeburn Media. All rights reserved.
+ */
+
+package net.watsonrolfes.fabulousflora.registry;
+
+import dev.architectury.registry.registries.DeferredRegister;
+import dev.architectury.registry.registries.RegistrySupplier;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.FlowerBlock;
+import net.minecraft.component.type.SuspiciousStewEffectsComponent;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+import net.watsonrolfes.fabulousflora.FabulousFlora;
+
+import java.util.function.Supplier;
+
+import static net.watsonrolfes.fabulousflora.FabulousFlora.MOD_ID;
+
+public class FFBlocks {
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(MOD_ID, RegistryKeys.BLOCK);
+    public static final DeferredRegister<Item> BLOCK_ITEMS = DeferredRegister.create(MOD_ID, RegistryKeys.ITEM);
+    public static final RegistrySupplier<Block> BLACK_ORCHID = BLOCKS.register("black_orchid", createFlowerBlock("black_orchid"));
+    public static final RegistrySupplier<Block> BLUE_CHRYSANTHEMUM = BLOCKS.register("blue_chrysanthemum", createFlowerBlock("blue_chrysanthemum"));
+    public static final RegistrySupplier<Block> BROWN_HYDRANGEA = BLOCKS.register("brown_hydrangea", createFlowerBlock("brown_hydrangea"));
+    public static final RegistrySupplier<Block> CYAN_POPPY = BLOCKS.register("cyan_poppy", createFlowerBlock("cyan_poppy"));
+    public static final RegistrySupplier<Block> GLOW_CALLA_LILY = BLOCKS.register("glow_calla_lilly", createFlowerBlock("glow_calla_lilly"));
+    public static final RegistrySupplier<Block> GRAY_MOGRA = BLOCKS.register("gray_mogra", createFlowerBlock("gray_mogra"));
+    public static final RegistrySupplier<Block> GREEN_ASTAR = BLOCKS.register("green_astar", createFlowerBlock("green_astar"));
+    public static final RegistrySupplier<Block> LIGHT_BLUE_ROSE = BLOCKS.register("light_blue_rose", createFlowerBlock("light_blue_rose"));
+    public static final RegistrySupplier<Block> LIGHT_GRAY_MORNING_GLORY = BLOCKS.register("light_gray_morning_glory", createFlowerBlock("light_gray_morning_glory"));
+    public static final RegistrySupplier<Block> LIME_TUBEROSE = BLOCKS.register("lime_tuberose", createFlowerBlock("lime_tuberose"));
+    public static final RegistrySupplier<Block> MAGENTA_SWEETPEA = BLOCKS.register("magenta_sweetpea", createFlowerBlock("magenta_sweetpea"));
+    public static final RegistrySupplier<Block> ORANGE_TULIP = BLOCKS.register("orange_tulip", createFlowerBlock("orange_tulip"));
+    public static final RegistrySupplier<Block> PINK_CHERRY_BLOSSOM = BLOCKS.register("pink_cherry_blossom", createFlowerBlock("pink_cherry_blossom"));
+    public static final RegistrySupplier<Block> PURPLE_IRIS = BLOCKS.register("purple_iris", createFlowerBlock("purple_iris"));
+    public static final RegistrySupplier<Block> RED_ARUM_LILY = BLOCKS.register("red_arum_lily", createFlowerBlock("red_arum_lily"));
+    public static final RegistrySupplier<Block> WHITE_LAVENDER = BLOCKS.register("white_lavender", createFlowerBlock("white_lavender"));
+    public static final RegistrySupplier<Block> YELLOW_BELL = BLOCKS.register("yellow_bell", createFlowerBlock("yellow_bell"));
+
+
+
+
+    public static void register() {
+        BLOCKS.register();
+        BLOCKS.forEach(blockRegistrySupplier -> {
+            Block block = blockRegistrySupplier.get();
+            BLOCK_ITEMS.register(blockRegistrySupplier.getIdAsString().replace("fabulous_flora:",""), createBlockItem(block));
+        });
+        BLOCK_ITEMS.register();
+    }
+
+    protected static Supplier<FlowerBlock> createFlowerBlock(String id) {
+        return () -> new FlowerBlock(SuspiciousStewEffectsComponent.DEFAULT, AbstractBlock.Settings.create().noCollision().breakInstantly().nonOpaque());
+    }
+
+    protected static Supplier<BlockItem> createBlockItem(Block block) {
+        return () -> new BlockItem(block,new Item.Settings().arch$tab(FabulousFlora.FF_CREATIVE_TAB));
+    }
+}
